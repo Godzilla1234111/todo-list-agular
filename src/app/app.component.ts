4fa7acb6388bb3b44d62ee9d39c136d2960928d5
Reza Fadaei <37462486+Godzilla1234111@users.noreply.github.com>
2023-02-07 04:03:01 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './Todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+
+  todos: Todo[] = [];
+  newTodo: string;
+
+  //saves items added to the list 
+  saveTodo() {
+    if (this.newTodo) {
+      let todo = new Todo();
+      todo.name = this.newTodo;
+      todo.isCompleted = true;
+      this.todos.push(todo);
+      this.newTodo = '';
+    } else {
+      alert("please enter somthing")
+    }
+  }
+  //togeling todo 
+  done(id:number){
+    this.todos[id].isCompleted = !this.todos[id].isCompleted;
+  }
+
+  //to temove items from the lsit
+
+  remove(id:number){
+    this.todos = this.todos.filter((v,i) => i !== id);
+
+  }
+
+
+
+
+
+
+
 }
+
